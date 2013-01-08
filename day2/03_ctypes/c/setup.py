c2e8c552576f94f41e8d571279701c4a0fbb1cc5
Chris Jordan-Squire <cjordan1@uw.edu>
2013-01-08 14:42:10 -0800

    arch compile not

--- day2/03_ctypes/c/setup.py
+++ day2/03_ctypes/c/setup.py
@@ -8,4 +8,15 @@ from distutils.core import setup, Extension
 #On linux this can be done with a command like
 #gcc -shared -fPIC -o foo.so kernel.c
 
+#However you might need to get some of the compile flags right,
+#if the python you're using was compiled with different options
+#or for a different architecture than is your default. 
+#(For example, I'm using the 32-bit EPD on my 64-bit mac)
+#To get the compile flags for the version of python you're running
+#call 
+#import sysconfig
+#sysconfig.get_config_vars()
+#That gives a (large) dictionary of configuration variables, 
+#including CFLAGS
+
 setup(name="foo", ext_modules=[Extension("foo", ['kernel.c'])] )
